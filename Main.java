555dd292dc774979b5221b63cd259fd885e15067
ikam <els0212@nate.com>
2021-04-19 17:35:40 +0900

    1197 / union & find, MST

--- Main.java
+++ Main.java
@@ -1,88 +1,97 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Gear gear = new Gear();
-		gear.getArgs();
-		gear.solve();
+		SpanningTree tree = new SpanningTree();
+		tree.getArgs();
+		tree.solve();
 	}
 }
 
-class Gear{
-	private String up, down;
-	
+class SpanningTree{
+	private int v, e;
+	private int sum = 0;
+	private LinkedList<Edge> edges = new LinkedList<Edge>();
+	private int parent[];
+	private int setCount[];
 	public void getArgs() {
 		Scanner scan = new Scanner(System.in);
-		up = scan.next();
-		down = scan.next();
+		this.v = scan.nextInt();
+		this.e = scan.nextInt();
+		for (int i = 0; i < this.e; i++) {
+			int st = scan.nextInt();
+			int ed = scan.nextInt();
+			int value = scan.nextInt();
+			edges.add(new Edge(st, ed, value));
+		}
+		parent = new int[this.v + 1];
+		setCount = new int[this.v + 1];
 		scan.close();
+		
 	}
-	
-	private static int getJ(int i, int upLen) {
-		// i가 up의 index 보다 크거나 같은 경우 index는 0부터 시작
-		if (i >= upLen - 1) {
-			return 0;
+	public int getParent(int child) {
+		if (parent[child] == child)
+			return child;
+		else {
+			parent[child] = getParent(parent[child]);
 		}
-		// i가 up의 길이보다 짧은 경우에는 j > 0인 지점부터 시작
-		else {// if (i < up.length() - 1) {
-			return upLen - i - 1;
-		}	
+		return parent[child];
 	}
-	
-	private static int getK(int i, int upLen) {
-		if (i >= upLen - 1)
-			return i - upLen + 1;
-		else
-			return 0;
-	}
-	
-	private boolean checkLine(int j, int k) {
-		for (; j < up.length() && k < down.length(); j++, k++) {
-			if (up.charAt(j) == '2' && down.charAt(k) == '2') {
-				return false;
-			}
+	public void merge(int src, int dst) {
+		int srcParent = parent[src];
+		int dstParent = parent[dst];
+		if (setCount[srcParent] >= setCount[dstParent]) {
+			parent[dst] = srcParent;
+			setCount[srcParent]++;
+			setCount[dstParent]--;
 		}
-		return true;
-	}
-	
-	public int check(String up, String down) {
-		int min = up.length() + down.length();
-		for (int i = 0; i <= up.length() + down.length() - 2; i++) {
-			// i가 down의 길이보다 작거나 같은 경우
-			if (i <= down.length() - 1) {
-				// j는 up의 index, k는 down의 index
-				int j = getJ(i, up.length());
-				int k = getK(i, up.length());
-				// 기어가 정상적으로 맞물리는 경우
-				if (checkLine(j, k) == true) {
-					int tempMin = i >= up.length() - 1 ? down.length() : up.length() + down.length() - i - 1;
-					if (min > tempMin)
-						min = tempMin;
-				}
-			}
-			// i가 down의 길이보다 긴 경우
-			else {
-				// j는 up의 index, k는 down의 index
-				int k = getK(i, up.length());
-				// 기어가 정상적으로 맞물리는 경우
-				if (checkLine(0, k)) {
-					if (min > i + 1) {
-						min = i + 1;
-					}
-				}
-			}
+		else {
+			parent[src] = dstParent;
+			setCount[dstParent]++;
+			setCount[srcParent]--;			
 		}
-		return min;
 	}
-
 	public void solve() {
-		if (up.length() > down.length()) {
-			String temp = this.up;
-			this.up = this.down;
-			this.down = temp;
+		edges.sort(null);
+		for (int i = 1; i <= v; i++) {
+			parent[i] = i;
+			setCount[i]++;
 		}
-		int min = check(up, down);
-		System.out.println(min);
+		
+		for (Edge edge: edges) {
+			int src = getParent(edge.getSt());
+			int dst = getParent(edge.getEd());
+			if (src != dst) {
+				sum += edge.getWeight();
+				merge(src, dst);
+			}
+		}
+		System.out.println(sum);
 	}
 }
 
+class Edge implements Comparable<Edge>{
+	private int st;
+	private int ed;
+	private int weight;
+	
+	public Edge(int st, int ed, int weight) {
+		this.st = st;
+		this.ed = ed;
+		this.weight = weight;
+	}
+	
+	public int getSt() {return this.st;}
+	public int getEd() {return this.ed;}
+	public int getWeight() {return this.weight;}
+	
+	@Override
+	public int compareTo(Edge edge) {
+		if (this.weight > edge.weight)
+			return 1;
+		else if (this.weight == edge.weight)
+			return 0;
+		return -1;
+	}
+}
